1b1626c0037141a13c88ec21333c44413eae64d3
syeony <fltkfltk1004@naver.com>
2024-11-16 16:02:05 +0900

    .

--- 2주차/1010.py
+++
@@ -1,8 +0,0 @@
-import math
-
-N=int(input())
-
-for i in range(0,N):
-    a, b = map(int, input().split())
-    combination = math.factorial(b) // (math.factorial(a) * math.factorial(b - a))
-    print(combination)
--- 3주차/11170.py
+++
@@ -1,8 +0,0 @@
-N=int(input())
-for i in range(0,N):
-    a,b=map(int,input().split())
-    sum=0
-    for j in range(a,b+1):
-        arr = list(map(int,str(j)))
-        sum+=arr.count(0)
-    print(sum)
