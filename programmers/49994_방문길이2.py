2d6c3580aa8c37913a584b68e4444bd5ae734e3e
syeony <fltkfltk1004@naver.com>
2024-11-19 00:30:32 +0900

    고침

--- programmers/49994_방문길이2.py
+++ programmers/49994_방문길이2.py
@@ -1,11 +1,10 @@
-# 내 코드에서 잘못된 점을 gpt에게 물어봄
-# 두 좌표를 배열이 아닌 튜플로 저장함
+# 내 코드에서 잘못된 점을 gpt에게 물어봐서 고침
 
 def solution(dirs):
     answer = 0
     dirs_dir = list(dirs.strip())
     x,y = 0,0
-    visited = set()
+    visited = []
     
     for dir in dirs_dir:
         nx = x
@@ -23,13 +22,13 @@ def solution(dirs):
         if nx>5 or ny>5 or nx<-5 or ny<-5:
             continue
         
-        if (x,y,nx,ny) not in visited and (nx,ny,x,y) not in visited:
-            visited.add((x,y,nx,ny))
-            visited.add((nx,ny,x,y))
+        if [x,y,nx,ny] not in visited and [nx,ny,x,y] not in visited:
+            visited.append([x,y,nx,ny])
+            visited.append([nx,ny,x,y])
             
         x = nx
         y = ny
-    
+            
     answer = len(visited)//2
     
     return answer
