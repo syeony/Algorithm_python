e8247962170fd711ec02ef8ee53780c6ae6aef05
syeony <fltkfltk1004@naver.com>
2024-11-27 14:21:44 +0900

    241127

--- programmers/42579_베스트앨범2.py
+++ programmers/42579_베스트앨범2.py
@@ -5,24 +5,18 @@
 
 def solution(genres, plays):
     answer = []
-    
-    # 딕셔너리 생성
     dic = {}
+
     for index, (genre, play) in enumerate(zip(genres, plays)):
         if genre not in dic:
             dic[genre] = []
-        # (재생 수, 고유 인덱스) 형태로 저장
         dic[genre].append([play, index])
     
-    # 장르별 재생 수를 기준으로 정렬
     for genre in dic:
-        # 재생 수 내림차순, 동일 재생 수일 경우 인덱스 오름차순으로 정렬
         dic[genre].sort(key=lambda x: x[0], reverse=True)
     
-    # 장르 총 재생 수를 기준으로 정렬
     dic = dict(sorted(dic.items(), key=lambda item: sum(play for play,index in item[1]), reverse=True))
     
-    # 각 장르에서 최대 두 곡 선택
     for key,value in dic.items():
         answer.append(value[0][1])
         if len(value) > 1:
