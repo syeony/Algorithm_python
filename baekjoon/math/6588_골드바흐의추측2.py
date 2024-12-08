6938c9ecb2ceafbef7ffb834adc39797841d88e0
syeony <fltkfltk1004@naver.com>
2024-12-08 13:48:09 +0900

    241208

--- baekjoon/math/6588_골드바흐의추측2.py
+++ baekjoon/math/6588_골드바흐의추측2.py
@@ -1,4 +1,8 @@
+# 문제 이해가 안되서 찾아본 블로그 (문제 이해용으로만 봄 답은 안봄)
+# https://greentea31.tistory.com/24
+
 import sys
+input = sys.stdin.readline
 
 sosu = [True] * 1000001 # 처음엔 모든 수가 전부 소수라고 가정
 sosu[0],sosu[1]=False,False # 0,1은 제외
@@ -9,15 +13,20 @@ for i in range(2,1000001):
     for j in range(i*i,1000001,i): # 2-> 4,6,8,10,...제외 (2로 나눠지니까 소수가 아님)
         sosu[j] = False
 
-while True:
-    num = int(sys.stdin.readline().rstrip())
-    if num == 0:
-        break
-    for i in range(3,num,2):
+n = int(input())
+partition = 0
+partition_arr = []
+
+for i in range(n):
+    num = int(input())
+    for i in range(num):
         if sosu[i] == True and sosu[num-i] == True:
-            print(f"{num} = {i} + {num-i}")
-            break
+            if (i or num-i) not in partition_arr:
+                partition_arr.append(i)
+                partition_arr.append(num-i)
+                partition += 1
         else:
             continue
-    else:
-        print("Goldbach's conjecture is wrong.")
+    print(partition)
+    partition = 0
+    partition_arr = []
