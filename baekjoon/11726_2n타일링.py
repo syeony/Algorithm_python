eff929c9b502cef1047d1f133660c4fe2f0e6abc
syeony <fltkfltk1004@naver.com>
2024-12-12 23:30:19 +0900

    241212

---
+++ baekjoon/11726_2n타일링.py
@@ -0,0 +1,16 @@
+# 아래 블로그 보고 dp이해
+# https://assb.tistory.com/entry/%EB%B0%B1%EC%A4%80-11726%EB%B2%88-2xn-%ED%83%80%EC%9D%BC%EB%A7%81
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * (n+1)
+
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3,n+1):
+    dp[i] = (dp[i-1]+dp[i-2])%10007
+
+print(dp[-1])
