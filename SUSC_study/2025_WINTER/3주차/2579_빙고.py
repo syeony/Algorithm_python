8183d8ee47aef0f9708c167a5245182ae2a2a55a
syeony <fltkfltk1004@naver.com>
2025-02-06 19:01:49 +0900

    0206

--- SUSC_study/2025_WINTER/3주차/2579_빙고.py
+++ SUSC_study/2025_WINTER/3주차/2579_빙고.py
@@ -8,20 +8,47 @@ for i in range(n):
     for j in range(n):
         arr3.append(arr2[i][j])
 
+result = 0
+index=0
 cnt=0
-def check(i,j):
-    # 구현
 
-while(cnt<3):
-    index=0
+def check():
+    global cnt
+    cnt=0
+
+    # 구현
+    for i in range(n):
+        if all(arr1[i][j]==0 for j in range(n)):
+            cnt+=1
+    
+    for j in range(n):
+        if all(arr1[i][j]==0 for i in range(n)):
+            cnt+=1
+
+    if all(arr1[i][i]==0 for i in range(n)):
+        cnt+=1
+
+    if all(arr1[i][n-i-1]==0 for i in range(n)):
+        cnt+=1
+
+
+while(index < len(arr3)):
     flag=False
+
     for i in range(n):
         for j in range(n):
             if arr1[i][j]==arr3[index]:
+                # print(arr3[index])
                 arr1[i][j]=0
-                cnt = check(i,j)
-                index+=1
-                flag=True
-                break
+                check()
+                if cnt==3:
+                    result=index+1
+                    flag=True
+                    index+=1
+                    break
         if flag:
             break
+    if flag:
+        break
+
+print(result)
